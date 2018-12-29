81cd1c04b5b9334ee3804c4facc377c6d19d5406
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2018-12-29 16:51:38 +0100

    Add pre-commit hook

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,16 +2,16 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
-    devServer: {
-        compress: true,
-        host: "0.0.0.0",
-        port: 9000,
-        open: true,
-        hot: true,
-        watchContentBase: true,
-        inline: true,
-        public: "http://localhost:9000"
-    }
+	mode: "development",
+	devtool: "inline-source-map",
+	devServer: {
+		compress: true,
+		host: "0.0.0.0",
+		port: 9000,
+		open: true,
+		hot: true,
+		watchContentBase: true,
+		inline: true,
+		public: "http://localhost:9000"
+	}
 });
